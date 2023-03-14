e343ff9cce65c5b7d352db21712c5b748f85f100
unknown <olawale.akinodanye@gmail.com>
2023-03-13 21:04:48 -0700

    Added some security middlewares

--- models/userModel.js
+++ models/userModel.js
@@ -53,6 +53,11 @@ const userSchema = new Schema({
     type: Date,
     select: false
   },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  },
   passwordResetToken: String,
   passwordResetExpires: Date
 });
@@ -69,6 +74,11 @@ userSchema.pre('save', function(next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  this.find({ active: true });
   next();
 });
 
